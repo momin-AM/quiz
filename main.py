4da6aea697dab5db38a70a180ec4d07786d45683
momin-AM <sk4549151@gmail.com>
2024-10-30 22:48:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,44 @@
+def quiz(q,a):
+    s=0
+    for j in range(len(q)):
+      print(f'question {j} : {q[j]}')
+      i=input('enter the answer(0 to stop): ')
+      if i=='0':
+          break
+      elif i.lower()==a[j]:
+          s=s+1
+          print('right answer\nyour score is: ',s)
+      else:
+          print('wrong answer. your total score is',s)
 
-# Python obfuscation by freecodingtools.org
-                    
-_ = lambda __ : __import__('zlib').decompress(__import__('base64').b64decode(__[::-1]));exec((_)(b'8Ijlf+3++//PqXNvhAajnP4WXX1LQHn2EpN2nQgfvUtWPkuDaC80WA5dvyoUKQZr3Y/gOCCgI4h1Hheg3oVa/2kiDrBsBzkQkphDHONRYyaI0EL/6EyZXQPgcFd5un1gOtlBBkDWxqUCj8SEQfsDT/erc91XtspycDdcGshOnhiEm3pjolsSm2MugPi8IQli2PBZvHvhF2YJByddqVZLWCF8MMIsACqX+X8AgfFyNFmDiiKSJawla0vWaUpA5ipI/AmWkk9b3c54Av9EuhtXOrvXiPciHlFdTMz+OPUpjvHzXhNCbe4W/jgRYy24kxvCppZzzHR90UWzzqW1Qut7af7oWxXmuw2v6byusFpzhULFeKziTJCSaFZdX7UD/rdVoYZPgLM1C98OMKTc2UcpDd4cEaJpM10MqvJB9JlYpjoXYKknW5BRydJtQp7tkvKPTsvaut4oCoOcjSdMz6gkVdhECfAJAWrjFlTbrgxp5xi2bQMYiHbov5vrupLUVwl8ncNc8P3mcOkmMlk5y6lMHt7h1RsAu1GGrBXEAH9YwWjbrwammAc22kKuFBJmrfgqLlorkxVeZqg9/cB86GXeTR4M0YQe+6ITf4qiG7SOY9bxGyjhrk/iwp+yyO+hpoe3DIMsgfrhzaObm/ttj5313nXY8/HXT7lA24/NUU/9BBZJP0NMPyf7E/5758EWrwia3iOCxpG2k72vrq1KkF+gIoTj0IKJkATRyUBQsKTZZn+zDfzGU1RwYJOY1y84HMRBVOksxeNaKVRDN20p8WPbsBjVYLRRWpmm+W6wGQ1rbP0A65V4CnFKH2Ox66gyM+NFY/0sM4QmGvurshh2fij5rw+99F2xiHomeh327/dew+ybarGCocXfC29m7P+KD5kWfqAaPaHy/rOx0oHYCEFwMpfwW4b087FJfWf1yVTDxkB5/odaOEqLIjMvqIy6bhACwF9nG/H946XYkkvMAG7TfAZtMmCrS1MXn0hSJHfqpIYFIpOfduPoqCrgXN/ck6vq1PgCLfb8tieAknpS4nqOojpMbEw5wLrON/DR1dWsPDFysrrL/XQ0sp3ioVYWiGM5aS7JcYknxt9KTH9Q0GLFOpIOyhgr2c+PT+AfHpGxz9VnWXp/fBFGT1CKsRVrg4GK9ZS8ZGlbr4fE6PjSomLeUGOiPnNDdCWXWEnUuHGl5ilT6dh3v5U+1CyKkp2uktsGx+LmEhE1yXMdHxX2N/47aeDNheJkxmGDXI+aZ7d2yWlJ0MT4tWiHzvKjUTi5VtSo/UvyWq6/jgHBmOmIb+y2f8M2QZsKV8LYnwHYMAeBjxs5T5PTBgcpXGRGsxvG/pOhy+XW4ZulSGm7nqT/vbJ7gSGd94x2EXJ0aEzIheFbsw4H9gyJ8qhgu9414sfdqpbjU9L24QYyZRWvKMNB2LIiZhkoQAChb2lYzDpn02605xD72Bw/oeF87MN8krW/fcK9jmPnXS1ILd+zlI+ZlrAV2h4pEICeU4GTdet09D8cotKn0ruOkdN8BceSe4bPXXSI9iNyR6Jn5uMwdOkcWSrGmMLDqMlAvPfTd/mVfvbVXt0W8CpkT8zgZFy53HmhkMbFMU2Qt604GZVOMrv/rzZcFqqx5uXcI+t96XmKLDfIB4XpJb0aF+PUtrB87OS8dh4wfehQd+XiL2uz1ZyIlw4KpbY6gi6EXTv+dp8KOj9C1B5eT25G3DxNsDOldlAh8/Ho8HvZTcEVqnIGQO3hQ+WqRJwISpOFFcPhwtnOUJGBtoL4c7jPM/FnGIGhz2/p0Y1Km0FrDpVhR4HaeXE/pkksvC5t2GV0eqinZe33P9XLTgxw4RUnCzeI0C/lkEajpRhmrWv0xTd+6V+2gBgvgTqojeiDtYqYOADJKR62Vg2GF8BPydjX3ccPEASHmobDWz/+yllCREd78OAZJIrGBZq3twbFHuPaHDEW+/X8H9uoMHUPsXLTjXZTuktTL0rQPOidyXNyiS2znRBYDGEED+87m4IwoUlymXp9ojO2//7tlXzmFGqW22i9TO7CcKFqp1yDU+Mmw+wPdwF9GwDOd0zjxubQd75HNsOU+cal++e4pUhauR8lZSOT2A3CQZbpY8vVLh/l3f4s07HkFQFDA+19knvgarTAeei0Pt6JzY8OE5B33Ucgl/4KdEkDFQeFAtSpTWj/U79l1Av2tVz8V1EJN8vV97A8TNajuVvrILv/hrRUj0cVbu4elMkyd3IBq14y7o6Ug1eaFSq0xNPLHVawDmoe0Kd/QEDjmcFdXfnmk4XlqFxHksDac3pgm7l3lQQyYyjMbTA+fUW0wNwsXNOBRcwyNU5sk7kDJXEt10E+54NF8i3O8lXGXbuuH1pLC3egTpd21GT/cUUYHzeZ8mvGTwCelgXeiSf9y8BfOE7Vt/XDUvwDfnaMXNMSAuqNmEI7Do4QLibJwML6yvp16lG/L43Gmy3H4OFgxMoPJ6qju5Qxv4EVm3w7Baw/M4HnXvANkMPASVg1Iv6Sf2b+/TMYJk1JemwaqHgmKzLfPqTiVxT4F1GIJRl9txAEvPHybLIcbQTggVGHcHTDWKk0qukOCq68g3cgkxjGaFHkyJ6rJ5b7D5FcBfPZ0eArBJGZZhYVffmOSz+D+BBDm6rRjRRCsvZAxpb6ws1o8EJN1hHufqrsu0I+KepOc7NRRxjszu7ZStWaeFuAI6nSf8t1z4D3fA9y5/wOct6oREibxPPr1qpCkKzAVHTLg9sY6Ah6OJMmtYR2cMMaFrTjqDG16amDOTlIg9oGwNGbXbLR6n436h0DK3uS2VqS2g+2Wuf7YXKT2n6stOok1mABNsZKakzvUkq0Wr+NlDwClmp11XaHhcFOnLSAVkVhFd7rEwmlk1Xdgwgb9+FnqAkwVO0WECBCIogW6Ho5j3zFP6iuiJhRm0MZYkMbzFA3+5oaEBINbaA6PO80gc0eFKmGysrOtiuglh7sHNNnJiBFqV0fgxCc33Bkc/gr7VzbT5LJS6Kc+z4MRY4qyhW4vfNTMvi8BPy0ifRk02wrRWNFe8pgYZv+NbaKDvR1gtDQTmlZfVWipmMp5zhMeYdizR7YVdrkJBW6zh1g6oRfJISC1jRJu/pAThtOTY8ukOy2GDdl0f/LxRmV7x2ru0spx+8nIBAI23JOqcezPliUSMPOlNovQn5UxGRL7dv23pGaKjKAxMDSv6snQOebVv2KcFHWFjiqfN5VZ5ySypTizjh4Y3yIkh9LeV80QfxGhGEDztzXirqoZQwzfShzUdTPXxRBuGUzfy7m7af+LwJWdmTHJ2FVvUwysGFsyd5pJx/pWV50fyWG86VT0Uc0nfoPGq4ZTaWImU0q1t5h4ZqDvCjWNRI19DeIYjgqcPf3880g3mI+Cja/QR+XhidgvM7A8x31pHR9ns7NH/ToKoZ8B8/HLZMav9H5jrGJ3dc30KjCR6bpyBvG624DBCVnZii0Uq+AMfpNWdJxRFfSM9TdcDDCilK36GAKCKlRqAfN7FMqj3AMaE7zDcIZtd+byoo02imiE8khppwq2UhVFW3vFf3p+bfd2Qg6SGGDC1vsy6Qf9jqVAWqmDbaB+J2J15ERdraXPH01XftcTfWh7cIjzJEmcvZU61uN2NA3f8NrbdX+gG9a6we0TiBFueoUNfuvi65p+lZkyvo8VUp8tk66KnL+I8nRdpdLWoqwFoevGnkhlOdEzocaEkfoxMkTzGCJXGYRvfX5xSpKFePXxnsbzLUa2+ZIqfCzcLhiyqYamYGdKcmZ7rVZDMkSb01s05ucU5sgDItoKrrC7isTqRque1hdBPcy0g45uEFbq+hgHjGQIrxJuA+CJfadrR3z9pZ3xpR+cFOC7yQ7GKrV90mK5lC7RVwwy4BXN2isdv7ZnRXiNzcRJW4JYCguXKFHZpmIklHIImhd5uubnHFFSl8y/nGN6RIsxTvvbBSk4xw2FfegEZ17A0AY6pI0mXr4uJQCnBzSYJofIYnedCaCrE3ed3HYDHm/RkoFEg3uYwMJXLwCsRIalUUh+H4vXZVuOi6FfnTdvcgSYzxAX9YMG16mM3NtUtvFAPyQXAfue3JpG8t75E0RMx5s91zvAl3jhBXLUvMELP/VwOZDl0EHf+yjd7HVKaOCDhXwHJM96gE5nr5pHgokBhgfrpN1ui74c0zSaKYxNBzpTyUxrK9MbdA5fFPTp5Ry9/sTSlnekDes1nTiD5a+wxnQ6qxzo1ojz7pXVJXT9ENBddpi4EvDAFw6iIrt0VSFx54bMK6Vt7G+J3w6DrnrftLlhr0rNHu/SwImR3OpfnFLjnGQEMbtBjsc7X76kZ2rpMuNGvY48Ipj4ZqHD1EeXvpQRHEkWNFUeDLkPTP2ECIqCliKNF/X5JXrKSn3o2iN7eSHM/r8Y+YtX9LmKr5Yvo86IvAG9vw8p30p00An6aNVbTfiGSGRImetOwSQWmq/+oQJ4HpmTapEsHFI4oio+98GwCQNReWRz1eiIwRR2QlSdsZ5dD7siFO6yQK8qEjFKEFApuFa+Ve8NQl5gFEVtT5hXP5/C2D8ImdkKaP+TjodStcOp4XrpxhMFwY/1lmYB7gU95qWMl8TYMmZVZ7Ma+EPm5Ff97PxxUnJ0aixz3xvCZZxCnlRjojbrSuFnhTZjcS50Rgm8eS3Os6zH/Znr0yyTp9wim4xbyapbQ+irYxkJBnxm+w7xj78dS+QaKGcMLoQkocxqdcP4xJ3rBkuSwCuPgCYuDuYVMTSm0/4tfCoRXFeOAxqX/luEW1RAGuadREmDLFoCmAJDGUoJC1b5/P5//7357/fKmq6Izl4lqAQvsv6TPzPm1xJzODM9YQmBGeVTfZRSgTxyW0lNwJe'))
+def qa():
+    inp=input('enter question : ')
+    with open('questions.txt','a') as f:
+        f.write(','+inp) 
+    inp=input('enter answer : ')
+    with open('answers.txt','a') as f1:
+        f1.write(','+inp)
+def loaddata():
+    with open('questions.txt','r') as f:
+        q=f.read().split(',')
+    
+    with open('answers.txt','r') as f1:
+        a=f1.read().split(',')
+    return q,a
+q,a=loaddata()
+while True:
+    inp1=int(input('1.quiz  or 2.write questions and answers(0 to stop): '))
+    if inp1==0:
+        break
+    elif inp1==1:
+        quiz(q,a)
+    elif inp1==2:
+        passwd=input('enter password : ')
+        if passwd=='momin0000':
+            qa()
+            print('(ques. and ans. saved successfully)')
+        else:
+            print('wrong password, ask\nhttps://www.facebook.com/profile.php?id=100044047537257\nfor password') 
+    else:
+        print('please choose 1 or 2 or 0')
+print('.\n.\nthanks for playing my quiz:)\n.\ncreated by >> momin')
